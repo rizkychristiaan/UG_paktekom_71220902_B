aa3a9b3409685ecc27187264fcdf71b6ec8d7226
rizkychristiaan <117886340+rizkychristiaan@users.noreply.github.com>
2022-12-01 13:04:47 +0700

    Add files via upload

---
+++ soal 1_TC1.py
@@ -0,0 +1,24 @@
+print("select operation")
+print("1.add")
+print("2.subtract")
+print("3.multiply")
+print("4.divide")
+pilihan=int(input("enter chice(1/2/3/4): 3"))
+def add(b,a):
+    k = b + a
+ retrun = k 
+def subtract(a,b):
+    c = a * b 
+    retrun = c
+def multiply(c,k):
+    b = c / k
+def divide(a,b):
+    c = a - b 
+    retrun = c 
+
+    if pilihan == 1: 
+        a = int(input("enter first number: ")) 
+        b = (int(input("enter second number: ")))
+   
+ 
+
